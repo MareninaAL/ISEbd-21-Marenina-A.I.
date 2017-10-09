d5b4ae589e0d2fe1df9837119d4915543c88e197
MareninaAL <31698119+MareninaAL@users.noreply.github.com>
2017-10-09 15:26:21 +0400

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        Color color;
+        Color dopColor;
+        int Weight;
+        int Price;
+        int MaxCountVolume; 
+
+        private Instrument inst;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            color = Color.Gray;
+            dopColor = Color.Yellow;
+            Weight = 1500;
+             Price = 15000; 
+            MaxCountVolume = 70; 
+            DopColorButton.BackColor = dopColor;
+            buttonSelectColor.BackColor = color;
+        }
+
+        private bool checkFields()
+        {
+
+            if (!int.TryParse(TextBoxWeight.Text, out Weight))
+            {
+                return false;
+            }
+            if (!int.TryParse(TextBoxPrice.Text, out Price))
+            {
+                return false;
+            }
+            if (!int.TryParse(textBoxMaxVolume.Text, out MaxCountVolume))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void newColorButton_Click_1(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+               dopColor = cd.Color;
+                DopColorButton.BackColor = dopColor;
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e) 
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                color = cd.Color;
+                buttonSelectColor.BackColor = color;
+
+            }
+
+        }
+
+        private void SetTrumpet_Click_1(object sender, EventArgs e)
+        {
+            if (checkFields())
+            {
+                inst = new Wind_Musical_Instrument(Weight, color, Price, MaxCountVolume);
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                inst.Draw_Wind_Instrument(gr);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+
+      
+
+
+        private void SetSaxophone_Click_1(object sender, EventArgs e)
+        {
+            if (checkFields())
+            {
+                inst = new Saxophone(Weight, color, dopColor, Price, MaxCountVolume,  checkBox1.Checked, checkBox2.Checked, checkBox3.Checked);
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                inst.Draw_Wind_Instrument(gr);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+
+
+       
+
+        private void Sound_Click(object sender, EventArgs e) 
+        {
+            if (inst != null)
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                inst.DrawDoing(gr);
+                pictureBox1.Image = bmp;
+
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            inst = new Saxophone(150, Color.Black, Color.Yellow, 10000, 40, true, true, true);
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            inst.Draw_Wind_Instrument(gr);
+            pictureBox1.Image = bmp;
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Saxophone.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+
+
+    public class Saxophone : Wind_Musical_Instrument
+    {
+        private Color dopColor;
+        private bool button ;
+        private bool bend; 
+        private bool mouthpiece; 
+
+        public Saxophone(double Weight, Color color, Color dopColor, int Price, int MaxCountVolume,  bool bend, bool button, bool mouthpiece) : base(Weight, color, Price, MaxCountVolume)
+        {
+            this.dopColor = dopColor; 
+            this.bend = bend;
+            this.button = button;
+            this.mouthpiece = mouthpiece; 
+        }
+
+
+        protected override void DrawTrumpet(Graphics g)
+        {
+          
+            base.DrawTrumpet(g);
+
+           
+            if (bend)
+            {
+                
+                Pen pen = new Pen(Color.Black);
+                g.DrawLine(pen, startPosX , startPosY+50, startPosX , startPosY + 80);
+                g.DrawArc(pen, startPosX, startPosY + 50, 45, 65,30,180);
+                g.DrawLine(pen, startPosX+43, startPosY + 94, startPosX+43, startPosY + 75);
+                g.DrawEllipse(pen, startPosX + 16, startPosY + 65, 28, 15);
+                g.DrawLine(pen, startPosX + 12, startPosY + 60, startPosX + 12, startPosY + 85);
+                g.DrawLine(pen, startPosX + 12, startPosY + 85, startPosX + 15, startPosY + 70);
+
+
+            }
+
+            if (button)
+            {
+                
+
+                 Brush spoiler = new SolidBrush(dopColor); 
+                g.FillEllipse(spoiler, startPosX +2, startPosY + 15 , 7 , 7);
+                g.FillEllipse(spoiler, startPosX + 2, startPosY + 29, 7, 7);
+                g.FillEllipse(spoiler, startPosX + 2, startPosY + 41, 7, 7);
+                g.FillEllipse(spoiler, startPosX + 9, startPosY + 15, 7, 7);
+                g.FillEllipse(spoiler, startPosX + 9, startPosY + 29, 7, 7);
+                g.FillEllipse(spoiler, startPosX + 9, startPosY + 41, 7, 7);
+
+                Pen pen = new Pen(Color.Black);
+                g.DrawEllipse(pen, startPosX + 2, startPosY + 15, 8, 8);
+                g.DrawEllipse(pen, startPosX + 2, startPosY + 29, 8, 8);
+                g.DrawEllipse(pen, startPosX + 2, startPosY + 41, 8, 8);
+                g.DrawEllipse(pen, startPosX + 9, startPosY + 15, 8, 8);
+                g.DrawEllipse(pen, startPosX + 9, startPosY + 29, 8, 8);
+                g.DrawEllipse(pen, startPosX + 9, startPosY + 41, 8, 8);
+            }
+
+            if (mouthpiece)
+            {
+                Pen pen = new Pen(Color.Black);
+
+                g.DrawLine(pen, startPosX+2 , startPosY -7 , startPosX -7, startPosY-23 );
+                g.DrawLine(pen, startPosX+8, startPosY-7, startPosX - 7, startPosY - 30);
+            }
+
+        }
+    }
+
+
+}
