fee538179190b5db0842777be55f68c01104cc6f
MareninaAL <31698119+MareninaAL@users.noreply.github.com>
2017-11-10 21:26:07 +0400

    Add files via upload

---
+++ WindowsFormsApp4/WindowsFormsApp1/Form2.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.listBoxLevels = new System.Windows.Forms.ListBox();
+            this.LevelDown = new System.Windows.Forms.Button();
+            this.LevelUp = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(789, 492);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(798, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "положить трубу на витрину";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(798, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "положить саксофон на витрину";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(795, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "взять покупку с места ";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(817, 259);
+            this.maskedTextBox1.Mask = "00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(841, 285);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Купить ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(798, 314);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(122, 178);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // listBoxLevels
+            // 
+            this.listBoxLevels.FormattingEnabled = true;
+            this.listBoxLevels.Location = new System.Drawing.Point(800, 0);
+            this.listBoxLevels.Name = "listBoxLevels";
+            this.listBoxLevels.Size = new System.Drawing.Size(120, 95);
+            this.listBoxLevels.TabIndex = 7;
+            // 
+            // LevelDown
+            // 
+            this.LevelDown.Location = new System.Drawing.Point(800, 112);
+            this.LevelDown.Name = "LevelDown";
+            this.LevelDown.Size = new System.Drawing.Size(40, 35);
+            this.LevelDown.TabIndex = 8;
+            this.LevelDown.Text = "<";
+            this.LevelDown.UseVisualStyleBackColor = true;
+            this.LevelDown.Click += new System.EventHandler(this.LevelDown_Click);
+            // 
+            // LevelUp
+            // 
+            this.LevelUp.Location = new System.Drawing.Point(869, 112);
+            this.LevelUp.Name = "LevelUp";
+            this.LevelUp.Size = new System.Drawing.Size(35, 35);
+            this.LevelUp.TabIndex = 9;
+            this.LevelUp.Text = ">";
+            this.LevelUp.UseVisualStyleBackColor = true;
+            this.LevelUp.Click += new System.EventHandler(this.LevelUp_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 492);
+            this.Controls.Add(this.LevelUp);
+            this.Controls.Add(this.LevelDown);
+            this.Controls.Add(this.listBoxLevels);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ListBox listBoxLevels;
+        private System.Windows.Forms.Button LevelDown;
+        private System.Windows.Forms.Button LevelUp;
+    }
+}
---
+++ WindowsFormsApp4/WindowsFormsApp1/Magazine.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Magazine
+    {
+
+        List<ClassArray<IInstrument>> magazineStages;  
+        ClassArray<IInstrument> magazine; 
+        int countPlaces = 12; 
+
+        int currentLevel;
+        
+        int placeSizeWidth = 210;
+        int placeSizeHeight = 120;
+
+      
+        public int GetcurrentLevel { get { return currentLevel; } }
+
+        public Magazine(int countStages) {
+       
+            magazineStages = new List<ClassArray<IInstrument>>(countStages); 
+        
+            for (int i=0; i<countStages;  i++)
+            {
+                magazine= new ClassArray<IInstrument>(countPlaces, null);
+                magazineStages.Add(magazine);
+            }
+            
+
+        }
+
+     
+        public void LevelUp()
+        {
+            if (currentLevel + 1 < magazineStages.Count)
+            {
+                currentLevel++;
+            }
+        }
+
+        public void LevelDown()
+        {
+            if (currentLevel > 0)
+            {
+                currentLevel--;
+            }
+
+        }
+
+
+
+        public int PutSaxophoneInMagazine(IInstrument Saxophone)
+        {
+           
+
+            return magazineStages[currentLevel] + Saxophone;
+
+        }
+
+        public IInstrument GetSaxophoneInMagazine(int ticket)
+        {
+            
+            return magazineStages[currentLevel] - ticket;
+        }
+
+
+
+        public void Draw (Graphics g, int width, int height)
+        {
+       
+            DrawMarking(g);
+            for (int i = 0; i < countPlaces; i++)
+            {
+                 var Saxophone = magazineStages[currentLevel][i];
+                if (Saxophone != null)
+                {
+                    Saxophone.SetPosition(4 + i / 4 * placeSizeWidth + 30, i % 4 * placeSizeHeight + 20);
+
+                    Saxophone.Draw_Wind_Instrument(g);
+                }
+            }
+
+
+        }
+
+        public void DrawMarking (Graphics g)
+        {
+            Pen pen = new Pen(Color.Black, 3);
+            g.DrawString("L" + (currentLevel+1), new Font ("Arial", 30), new SolidBrush(Color.Black), (countPlaces/4)*placeSizeWidth-80, 420);
+            g.DrawRectangle(pen,0,0,(countPlaces/4)*placeSizeWidth-100,480);
+            for (int i=0; i<countPlaces/4 ;i++)
+            {
+                for (int j=0; j<5; ++j)
+                {
+                    g.DrawLine(pen, i*placeSizeWidth, j*placeSizeHeight, i * placeSizeWidth +110 , j * placeSizeHeight);
+                    if (j < 4)
+                    {
+
+                        g.DrawString((i * 4 + j ).ToString(), new Font("Arial", 30), new SolidBrush(Color.Black), i * placeSizeWidth + 60, j * placeSizeHeight + 0);
+
+                    }
+                }
+              
+                g.DrawLine(pen, i * placeSizeWidth+110, 0, i * placeSizeWidth+110, 400);
+            }
+        }
+    }
+}
