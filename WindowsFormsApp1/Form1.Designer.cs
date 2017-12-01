637ac389ac63d926483f90cec70a40a0c8398abc
MareninaAL <31698119+MareninaAL@users.noreply.github.com>
2017-12-01 20:29:13 +0400

    Update Form1.Designer.cs

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -206,13 +206,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(410, 357);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(142, 23);
-            this.button1.TabIndex = 17;
-            this.button1.Text = "показать сразу";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+//             this.button1.Location = new System.Drawing.Point(410, 357);
+//             this.button1.Name = "button1";
+//             this.button1.Size = new System.Drawing.Size(142, 23);
+//             this.button1.TabIndex = 17;
+//             this.button1.Text = "показать сразу";
+//             this.button1.UseVisualStyleBackColor = true;
+//             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Form1
             // 
